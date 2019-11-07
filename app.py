3c0274ec5414d68e7e4cf6543c3010d1d43049f3
one-color-low <i1931011@uec.ac.jp>
2019-11-08 00:25:48 +0900

    音ズレ改善

--- app.py
+++ app.py
@@ -141,11 +141,13 @@ def makevmd():  # todo: できれば名前変えたい(音声変換もするの�
         wav, _ = librosa.load(wav_path)
         vc_result = VoiceConverter.convert_voice(wav)
         librosa.output.write_wav(processed_wav_path, vc_result, sr=22050)
+        # todo: 保存するprocessed_wav_pathをVroom.htmlからの相対パスに
 
         ## 動画変換
         ### input: fps30_mp4_path, output: vmd_path
         vmd_path = app.config['STATIC_FOLDER']+'/vmds/'+request.args.get('room_name','')+'.vmd'
         vmdlifting.vmdlifting(fps30_mp4_path,  vmd_path)
+        # todo: 保存するvmd_pathをVroom.htmlからの相対パスに
 
 
         # 音声変換処理で返ってきたパス(processed_wav_path)と
