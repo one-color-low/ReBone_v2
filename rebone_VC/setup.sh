5db14773919e8db625e7c9e85193c1864ec6b52b
one-color-low <i1931011@uec.ac.jp>
2019-11-15 20:08:31 +0900

    pretrain_dataの取得元を変更

--- rebone_VC/setup.sh
+++ rebone_VC/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo 'Downloading from googledrive...'
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Yu8BMJkFTv8AR26R0yNk7KPNNUdosT42" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1NnBiuK7PH33NGx-HlD6vbHepZ6NS0oFS" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Yu8BMJkFTv8AR26R0yNk7KPNNUdosT42" -o pretrain_data.zip
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1NnBiuK7PH33NGx-HlD6vbHepZ6NS0oFS" -o pretrain_data.zip
 
 echo 'Done'
