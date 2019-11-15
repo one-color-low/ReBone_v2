8de5f41f15be332d88f51475797a136dec296394
one-color-low <i1931011@uec.ac.jp>
2019-11-15 17:16:12 +0900

    setup.sh修正

--- pose_est_mod/setup.sh
+++ pose_est_mod/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo 'Downloading from googledrive...'
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1TDJCFZ5xtTAZcxkKFrXeStDkIzaQq" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1TDJCFZ5xtTAZcxkKFrXeStDkIzaQq-x4" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1TDJCFZ5xtTAZcxkKFrXeStDkIzaQq" -o pose_est_mod_data.zip
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1TDJCFZ5xtTAZcxkKFrXeStDkIzaQq-x4" -o pose_est_mod_data.zip
 
 echo 'Done'
