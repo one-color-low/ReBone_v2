8960e47ed2cf74720356afdce392956653fab116
one-color-low <i1931011@uec.ac.jp>
2019-11-15 17:09:40 +0900

    pose_est_modの大きいデータを持ってくるshを追加

---
+++ pose_est_mod/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'Downloading from googledrive...'
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1TDJCFZ5xtTAZcxkKFrXeStDkIzaQq" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1TDJCFZ5xtTAZcxkKFrXeStDkIzaQq" -o pose_est_mod_data.zip
+
+echo 'Done'
